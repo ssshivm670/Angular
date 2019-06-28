de9af7d5fa71523471ecc1b33979f9045b770707
Shivam Sharma <Shivam.Sharma@thomsonreuters.com>
2019-06-28 13:08:50 +0530

    incomplete Angular tutorial

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ install angular cli
 create angular project
     ng new ng6-test --style=scss --routing
 run server and open browser
-    npm serve -o
+    ng serve -o
     
 generate component
     ng generate component sidebar
+    ng g c posts
+    
+generate a service
+    ng generate service data
+
--- ng6-test/src/app/app.module.ts
+++ ng6-test/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
 
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    DetailsComponent,
+    PostsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
